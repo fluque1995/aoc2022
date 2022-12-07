78bbae1ec90cd846d9b9a8dd5e707fe40e578928
Francisco Luque Sánchez <fluque1995@gmail.com>
2022-12-07 19:11:43 +0100

    Removed space

--- day7.py
+++ day7.py
@@ -35,10 +35,11 @@ def problem_1():
 
 def problem_2():
     paths = read_input()
-    unused_space = 70000000  - paths['/']
+    unused_space = 70000000 - paths['/']
     needed_space = 30000000 - unused_space
     return min(filter(lambda x: x > needed_space, paths.values()))
 
 
-problem_1()
-problem_2()
+if __name__ == '__main__':
+    print(problem_1())
+    print(problem_2())
